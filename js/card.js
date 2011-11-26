76283f7111ef323d0a55a187cdde8e84a39c00f2
Remco Veldkamp <remcoder@gmail.com>
2011-11-26 19:36:42 +0100

    some architecture

--- js/card.js
+++ js/card.js
@@ -1,45 +1,48 @@
-function Card(id, url, row, column, rows, cols) {
-	console.log(	console.logid, url, row, column, rows, cols);
-	this.id = id;
-	this.url = url;
+function Card(obj, memory) {
+	//console.log(	console.logid, url, row, column, rows, cols);
 
-	this.$card = $("<div>").addClass("card");
-	var $wrapper = $("<div>").addClass("wrapper").addClass("front");
-	var $front = $("<img>").attr("src", url + "&100x100");
-	$wrapper.append($front);
-	var $back = $("<div>").addClass("back");
-
-	this.$card.append($wrapper);
-	this.$card.append($back);
-
-	this.sprite = new Sprite3D(this.$card[0]);
-
-	Sprites.push(this.sprite);
-	Stage.addChild(this.sprite);
-
-	this.startPos = {
-		x : 50 + 100 * (column-cols/2),
-		y : 50 + 100 * (row-rows/2)
-	}
-
-	this.sprite
-			.setRegistrationPoint( 50, 50, 0 )
-			.setPosition(this.startPos.x, this.startPos.y, 0)
-			.setRotation(0, 180, 0)
-			.update();
-
-	this.found = false;
-
-	this.$card.click($.proxy(function (evt) {
-		evt.preventDefault();
-		console.log("click");
-		this.onClick(evt);
-	}, this));
-
+	this.obj = obj;
+	this.memory = memory;
 }
 
 Card.prototype = {
+	display: function (row, column, rows, cols) {
+		this.$card = $("<div>").addClass("card");
+		var $wrapper = $("<div>").addClass("wrapper").addClass("front");
+		var $front = $("<img>").attr("src", this.obj.url + "&100x100");
+		$wrapper.append($front);
+		var $back = $("<div>").addClass("back");
+
+		this.$card.append($wrapper);
+		this.$card.append($back);
+
+		this.sprite = new Sprite3D(this.$card[0]);
+
+		Sprites.push(this.sprite);
+		Stage.addChild(this.sprite);
+
+		this.startPos = {
+			x : 50 + 100 * (column-cols/2),
+			y : 50 + 100 * (row-rows/2)
+		}
+
+		this.sprite
+				.setRegistrationPoint( 50, 50, 0 )
+				.setPosition(this.startPos.x, this.startPos.y, 0)
+				.setRotation(0, 180, 0)
+				.update();
+
+		this.found = false;
+
+		this.$card.click($.proxy(function (evt) {
+			evt.preventDefault();
+			console.log("click");
+			this.onClick(evt);
+		}, this));
+	},
+
 	onClick : function () {
+		//if (this.obj.found)
 		this.flip();
 	},
 
--- js/memory.js
+++ js/memory.js
@@ -2,10 +2,30 @@ function Memory(cards) {
 	this.lives = 5;
 	this.buffer = "";
 	this.cards = cards;
+	this.moves = 0;
 
-
+	this.display();
 }
 
 Memory.prototype = {
+	display: function () {
+		this.cards.sort(function (){
+			return (Math.round(Math.random())-0.5);
+		});
 
+		this.cols = 5;
+		this.rows = ~~(this.cards.length / this.cols);
+
+		// display cards
+		for(var c=0 ; c<this.cards.length ; c++)
+		{
+			var row = ~~(c/this.cols);
+			var col = c % this.cols;
+
+			var card = this.cards[c];
+
+			card.display(row,col, this.rows, this.cols);
+
+		}
+	}
 }
